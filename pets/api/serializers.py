8ebf7042a5a4e4b71428456cbf8409d190d8cba8
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2023-03-09 13:22:48 +0300

    Add files via upload

--- pets/api/serializers.py
+++ pets/api/serializers.py
@@ -1,27 +1,27 @@
 from rest_framework import serializers
 from .models import Pet, Type, Stat, Order, Category
 
-class PetSerialiser(serializers.Serializer):
+class PetSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Pet
         fields = '__all__'
 
-class TypeSerialiser(serializers.Serializer):
+class TypeSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Type
         fields = '__all__'
 
-class StatSerialiser(serializers.Serializer):
+class StatSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Stat
         fields = '__all__'
 
-class OrderSerialiser(serializers.Serializer):
+class OrderSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
-class CategorySerialiser(serializers.Serializer):
+class CategorySerialiser(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
